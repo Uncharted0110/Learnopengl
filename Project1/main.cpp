45970ac467f698095dc4e3d5b89aef1a4163a20c
Uncharted0110 <adityakl1509@gmail.com>
2025-06-30 20:08:45 +0530

    10 f*ing cubes

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -83,6 +83,19 @@ float vertices[] = {
         -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
 };
 
+glm::vec3 cubePositions[] = {
+    glm::vec3(-2.0f, 4.0f, -15.0f),
+    glm::vec3(2.4f,  2.0f, -11.0f),
+    glm::vec3(-3.5f, -1.2f, -4.5f),
+    glm::vec3(-3.8f, -2.0f, -12.3f),
+    glm::vec3(2.4f, -0.4f, -3.5f),
+    glm::vec3(-1.7f,  3.0f, -7.5f),
+    glm::vec3(1.3f, -2.0f, -2.5f),
+    glm::vec3(1.5f,  2.0f, -2.5f),
+    glm::vec3(1.5f,  0.2f, -1.5f),
+    glm::vec3(-1.3f,  1.0f, -1.5f)
+};
+
 int main()
 {
     glfwInit();
@@ -197,14 +210,9 @@ int main()
         glClearColor(0.4f, 0.3f, 0.7f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-
-        glm::mat4 model = glm::mat4(1.0f);
-        model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
-
-        
+     
         ourShader.use();
         ourShader.setFloat("mixValue", sin(glfwGetTime() * 10.0f));
-        ourShader.set4Matrix("model", model);
         
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, texture1);
@@ -212,7 +220,16 @@ int main()
         glBindTexture(GL_TEXTURE_2D, texture2);
 
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 36);
+        for (unsigned int i = 0; i < 10; i++)
+        {
+            glm::mat4 model = glm::mat4(1.0f);
+            model = glm::translate(model, cubePositions[i]);
+            float angle = 20.0 * i;
+            model = glm::rotate(model, (float)glfwGetTime() * glm::radians(angle), glm::vec3(0.5f, 1.0f, 0.0f));
+            ourShader.set4Matrix("model", model);
+
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
 
         // check and call events and swap buffer
         glfwSwapBuffers(window);
